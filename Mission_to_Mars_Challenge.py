7329b04ce4a706f19a82140666db0ca9ac7b3459
Lucas <lucasbratland@gmail.com>
2022-03-08 18:11:11 -0600

    updating readme

--- Mission_to_Mars_Challenge.py
+++ Mission_to_Mars_Challenge.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
@@ -11,7 +9,6 @@ import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# In[2]:
 
 
 # Set the executable path and initialize Splinter
@@ -21,7 +18,6 @@ browser = Browser('chrome', **executable_path, headless=False)
 
 # ### Visit the NASA Mars News Site
 
-# In[3]:
 
 
 # Visit the mars nasa news site
@@ -32,7 +28,6 @@ browser.visit(url)
 browser.is_element_present_by_css('div.list_text', wait_time=1)
 
 
-# In[4]:
 
 
 # Convert the browser html to a soup object and then quit the browser
@@ -42,21 +37,11 @@ news_soup = soup(html, 'html.parser')
 slide_elem = news_soup.select_one('div.list_text')
 
 
-# In[5]:
-
-
 slide_elem.find('div', class_='content_title')
-
-
-# In[6]:
-
 
 # Use the parent element to find the first a tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
-
-
-# In[7]:
 
 
 # Use the parent element to find the paragraph text
@@ -66,15 +51,10 @@ news_p
 
 # ### JPL Space Images Featured Image
 
-# In[8]:
-
 
 # Visit URL
 url = 'https://spaceimages-mars.com'
 browser.visit(url)
-
-
-# In[9]:
 
 
 # Find and click the full image button
@@ -82,7 +62,6 @@ full_image_elem = browser.find_by_tag('button')[1]
 full_image_elem.click()
 
 
-# In[10]:
 
 
 # Parse the resulting html with soup
@@ -91,15 +70,11 @@ img_soup = soup(html, 'html.parser')
 img_soup
 
 
-# In[11]:
-
 
 # find the relative image url
 img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 img_url_rel
 
-
-# In[12]:
 
 
 # Use the base url to create an absolute url
@@ -109,22 +84,14 @@ img_url
 
 # ### Mars Facts
 
-# In[13]:
-
 
 df = pd.read_html('https://galaxyfacts-mars.com')[0]
 df.head()
 
 
-# In[14]:
-
-
 df.columns=['Description', 'Mars', 'Earth']
 df.set_index('Description', inplace=True)
 df
-
-
-# In[15]:
 
 
 df.to_html()
@@ -134,21 +101,14 @@ df.to_html()
 
 # ### Hemispheres
 
-# In[16]:
-
 
 # 1. Use browser to visit the URL 
 url = 'https://marshemispheres.com/'
 
 browser.visit(url)
 
-
-# In[17]:
-
-
 # 2. Create a list to hold the images and titles.
 hemisphere_image_urls = []
-
 
 # 3. Write code to retrieve the image urls and titles for each hemisphere.
 for hemi in range(4):
@@ -170,30 +130,9 @@ for hemi in range(4):
         
     browser.back()
     
-
-
-# In[18]:
-
-
 # 4. Print the list that holds the dictionary of each image url and title.
 hemisphere_image_urls
 
 
-# In[19]:
-
-
 # 5. Quit the browser
 browser.quit()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Mission-to-Mars
+
+## Overview
+This project consisted on a Python script to scrape text and images from various websites that talked about Mission to Mars. We then used a Flask web application with a rendered HTML template designed using Bootstrap to display all the data in a central location without having to gather it manually. The data scraped and displayed was stored in a non-relational Mongo database. Additionally, I was able to connect the scraping script so that each time a button was clicked, the scraping occured once again, the database got updated, and new data was displayed.
+
+## Software
+- Python
+- Jupyter Notebook
+- MongoDB
+- HTML5
+- Bootstrap 3
